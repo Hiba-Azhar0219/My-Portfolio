4d42b9049857bb7e26b4e0b16d324142a54abebf
Hiba-Azhar0219 <hiba.officialwork@gmail.com>
2025-03-18 22:20:54 +0500

    projects completed, footer is left

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ var mainPageContent = document.querySelector('.main-page-content')
 setTimeout(() => {
   mainPageContent.style.visibility = 'visible'
 }, 3000)
+
+// Select all project cards
+var projectCards = document.querySelectorAll('.project-card')
+
+projectCards.forEach((projectCard) => {
+  var projectCardImg = projectCard.querySelector('img')
+  var projectCardH3 = projectCard.querySelector('h3')
+
+  projectCard.addEventListener('mouseover', () => {
+    projectCardImg.style.opacity = '0'
+    projectCardH3.style.opacity = '1'
+  })
+
+  projectCard.addEventListener('mouseout', () => {
+    projectCardImg.style.opacity = '1'
+    projectCardH3.style.opacity = '0'
+  })
+})
