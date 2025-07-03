134f60327c3e636fec6da66378a6bf034833edc4
Hiba-Azhar0219 <hiba.officialwork@gmail.com>
2025-07-03 14:57:10 +0500

    Services and Resume Modified

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
-//mainpage
+// //mainpage
+// var mainPageContent = document.querySelector('.main-page-content')
+
+// setTimeout(() => {
+//   mainPageContent.style.visibility = 'visible'
+// }, 3000)
+
+// // Select all project cards
+// var projectCards = document.querySelectorAll('.project-card')
+
+// projectCards.forEach((projectCard) => {
+//   var projectCardImg = projectCard.querySelector('img')
+//   var projectCardH3 = projectCard.querySelector('h3')
+
+//   projectCard.addEventListener('mouseover', () => {
+//     projectCardImg.style.opacity = '0'
+//     projectCardH3.style.opacity = '1'
+//   })
+
+//   projectCard.addEventListener('mouseout', () => {
+//     projectCardImg.style.opacity = '1'
+//     projectCardH3.style.opacity = '0'
+//   })
+// })
+// mainpage
+// mainpage
 var mainPageContent = document.querySelector('.main-page-content')
 
 setTimeout(() => {
   mainPageContent.style.visibility = 'visible'
 }, 3000)
 
-// Select all project cards
-var projectCards = document.querySelectorAll('.project-card')
+const cardContainer = document.querySelector('.card-container')
+const cards = cardContainer.children
+let scrollSpeed = 1 // Adjust speed (pixels per frame)
 
-projectCards.forEach((projectCard) => {
-  var projectCardImg = projectCard.querySelector('img')
-  var projectCardH3 = projectCard.querySelector('h3')
+function animateSlider() {
+  cardContainer.scrollLeft += scrollSpeed
 
-  projectCard.addEventListener('mouseover', () => {
-    projectCardImg.style.opacity = '0'
-    projectCardH3.style.opacity = '1'
-  })
+  // Check if first card is completely out of view
+  const firstCard = cards[0]
+  const firstCardWidth = firstCard.offsetWidth + 20 // +gap between cards
 
-  projectCard.addEventListener('mouseout', () => {
-    projectCardImg.style.opacity = '1'
-    projectCardH3.style.opacity = '0'
-  })
-})
+  if (cardContainer.scrollLeft >= firstCardWidth) {
+    // Move first card to the end
+    cardContainer.appendChild(firstCard)
+    // Adjust scrollLeft to account for moved card
+    cardContainer.scrollLeft -= firstCardWidth
+  }
+
+  requestAnimationFrame(animateSlider)
+}
+
+// Start animation
+animateSlider()
